07416a911e04340b105688426a4de19b4da49bfc
DouiJ <141159120+DouiJ@users.noreply.github.com>
2025-02-23 14:47:55 +0800

    Add files via upload
    Profile Panel - tbd CSS styles of order history

---
+++ MP - PANELS/database_conn.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = "root";
+$password = "";
+$database = "tblstudent";
+$conn = new mysqli("localhost", $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
