6475cb589771fb656d495564dabb249551a934d1
rngrg <rianecaimoy>
2025-03-27 17:55:55 +0800

    New layout

--- Webpages/product.php
+++ Webpages/product.php
@@ -57,22 +57,11 @@
     <link href="https://fonts.googleapis.com/css?family=Newsreader&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
     <style>
-        .alt, .noadd, #max {
+        #noadd, #max {
             display: none;
         }
-        @media only screen and (max-width: 900px) {
-            .wide {
-                display: none;
-            }
-            .alt {
-                display: block;
-            }
-            .price {
-            padding: 0;
-        }
-        }
         body {
-            margin: 2% 5%;
+            margin: 2%;
             font-family: Inter;
         }
         li {
@@ -81,7 +70,6 @@
             padding: 14px 16px;
         }
         .logo, .price, h1, #quantity {
-            margin: 0;
             font-family: Newsreader;
             font-size: xx-large;
         }
@@ -90,6 +78,7 @@
         }
         .right {
             float: right;
+            margin-left: 5%;
         }
         .button {
             background-color: #426b1f;
@@ -105,9 +94,6 @@
             padding: 0;
             overflow: hidden;
         }
-        nav li {
-            margin-left: 5%;
-        }
         #product li, p.alt {
             margin-left: 1%;
             margin-top: 3%;
@@ -119,14 +105,33 @@
             text-decoration: none;
             color: black;
         }
-        header, section {
-            margin-top: 50px;
+        section {
+            margin-top: 2%;
         }
-        header, h1, h2, #empty, #quantity {
+        #product {
+            display: flex;
+            justify-content: center;
+        }
+        #product div {
+            width: 40%;
+            margin: 0 5%;
+        }
+        @media only screen and (max-width: 900px) {
+            #product {
+                flex-direction: column;
+            }
+            #product div {
+                width: 100%;
+                margin: 0;
+            }
+        }
+        h2, #empty, #quantity {
             text-align: center;
         }
         img {
             width: 100%;
+            border: 5px solid #426b1f;
+            border-radius: 10px;
         }
         .image {
             position: relative;
@@ -135,7 +140,7 @@
             position: absolute;
             display: inline;
             background-color: red;
-            font-size: 80px;
+            font-size: 50px;
             text-align: center;
             color: white;
             width: 100%;
@@ -156,12 +161,13 @@
             color: red;
         }
         #quantity {
-            width: 3%;
+            width: 5%;
         }
         h2 {
             font-family: Newsreader;
+            margin: 0;
         }
-        .noadd {
+        #noadd {
             background-color: black;
         }
     </style>
@@ -175,7 +181,7 @@
             <li class="right"><a href="#contact">Account</a></li>
         </ul>
     </nav>
-    <header>
+    <!--<header>
         <h1><?php echo $conn->query($info)->fetch_assoc()["name"]; ?></h1>
         <p><?php
             if ($r_num) {
@@ -186,15 +192,16 @@
             }
             else
                 echo "Not yet rated";
-        ?></p> 
-    </header>
+        ?></p>
+    </header>-->
     <section id="product">
         <div class="image">
             <img src="<?php echo $conn->query($img)->fetch_assoc()["file_path"]; ?>" alt="A close-up view of the shoe">
             <span id="soldout">SOLD OUT</span>
         </div>
+        <div>
+        <!--
         <ul>
-            <li class="price">PHP <?php echo $conn->query($info)->fetch_assoc()["price"]; ?></li>
             <li class="wide button add">Add to Cart</li>
             <li class="wide button noadd">Sold Out</li>
             <li class="wide inv"><span class="sales"></span> sold, <span class="stock"></span> in stock</li>
@@ -203,17 +210,29 @@
                     echo "<li class=\"wide button size right\">".$size[$i]."</li>";
             ?>
             <li class="wide right">Select a size</li>
-        </ul>
+        </ul>-->
+        <h1><?php echo $conn->query($info)->fetch_assoc()["name"]; ?></h1>
+        <p><?php
+            if ($r_num) {
+                echo $avg."/5 (".$r_num." review";
+                if ($r_num > 1)
+                    echo "s";
+                echo ")";
+            }
+            else
+                echo "Not yet rated";
+        ?></p>
+        <p class="price">PHP <?php echo $conn->query($info)->fetch_assoc()["price"]; ?></p>
         <ul class="alt">
-            <li class="button add">Add to Cart</li>
-            <li class="button noadd">Sold Out</li>
-            <li class="inv1"><span class="sales"></span> sold, <span class="stock"></span> in stock</li>
+            <li class="button" id="add">Add to Cart</li>
+            <li class="button" id="noadd">Sold Out</li>
+            <li class="inv"><span id="sales"></span> sold, <span id="stock"></span> in stock</li>
         </ul>
         <p class="alt">Select a size</p>
         <ul class="alt">
             <?php
                 for ($i = 0; $i < count($size); $i++)
-                    echo "<li class=\"button size1\">".$size[$i]."</li>";
+                    echo "<li class=\"button size\">".$size[$i]."</li>";
             ?>
         </ul>
         <ul id="qtyselect">
@@ -258,13 +277,12 @@
         ?>null];
 
         // DOM
-        const a = document.getElementsByClassName("add");
-        const i = document.getElementsByClassName("sales");
-        const i1 = document.getElementsByClassName("stock");
+        const a = document.getElementById("add");
+        const i = document.getElementById("sales");
+        const i1 = document.getElementById("stock");
         const m = document.getElementById("max");
-        const n = document.getElementsByClassName("noadd");
+        const n = document.getElementById("noadd");
         const s = document.getElementsByClassName("size");
-        const s1 = document.getElementsByClassName("size1");
         const so = document.getElementById("soldout");
         const q = document.getElementsByClassName("quantity");
         const qd = document.getElementById("quantity");
@@ -272,80 +290,67 @@
         const s_len = s.length;
 
         // Event listeners
-        for (let j = 0; j < 2; j++) {
-            a[j].addEventListener("click", function() {
-                addToCart();
-            });
-        }
+        a.addEventListener("click", function() {
+            alert("Adding to cart is not yet implemented. Size is " + selected_size);
+        });
 
         for (let j = 0; j < s_len; j++) {
             s[j].addEventListener("click", function() {
-                setSize(s_len - 1 - j);
-            });
-            s1[j].addEventListener("click", function() {
                 setSize(j);
             });
         }
 
         q[0].addEventListener("click", function() {
-            if (qty < stock[selected_size]) {
-                qty++;
+            qty++;
+            if (qty <= stock[selected_size]) {
                 qd.innerHTML = qty;
+                q[1].style.display = "";
             }
-            else
+            if (qty == stock[selected_size]) {
                 m.style.display = "block";
+                q[0].style.display = "none";
+            }
         });
         q[1].addEventListener("click", function() {
+            qty--;
             m.style.display = "";
-            if (qty > 1) {
-                qty--;
+            if (qty >= 1) {
                 qd.innerHTML = qty;
+                q[0].style.display = "";
             }
+            if (qty == 1)
+                q[1].style.display = "none";
         });
 
-        // Button functions
         function setSize(size) {
             selected_size = size;
             qty = 1;
             qd.innerHTML = qty;
             m.style.display = "";
 
-            for (let j = 0; j < 2; j++) {
-                i[j].innerHTML = sales[size];
-                i1[j].innerHTML = stock[size];
-            }
+            i.innerHTML = sales[size];
+            i1.innerHTML = stock[size];
 
             if (!stock[size]) {
                 so.style.display = "block";
                 qs.style.display = "none";
-                for (let j = 0; j < 2; j++) {
-                    a[j].style.display = "none";
-                    n[j].style.display = "block";
-                }
+                a.style.display = "none";
+                n.style.display = "block";
             }
             else {
                 so.style.display = "none";
                 qs.style.display = "";
-                for (let j = 0; j < 2; j++) {
-                    a[j].style.display = "";
-                    n[j].style.display = "";
-                }
+                a.style.display = "";
+                n.style.display = "";
+                q[1].style.display = "none";
             }
 
             for (let j = 0; j < s_len; j++) {
-                if (j == size) {
-                    s[s_len - 1 - j].style.backgroundColor = "black";
-                    s1[j].style.backgroundColor = "black";
-                }
-                else {
-                    s[s_len - 1 - j].style.backgroundColor = "";
-                    s1[j].style.backgroundColor = "";
-                }
+                if (j == size)
+                    s[j].style.backgroundColor = "black";
+                else
+                    s[j].style.backgroundColor = "";
             }
-        }
-
-        function addToCart() {
-            alert("Adding to cart is not yet implemented. Size is " + selected_size);
         }
 
         setSize(0);
